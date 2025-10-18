f5793a47262fb693d4fb124c67f2f1bb975b9b32
Paulo Junior <73448096+N7z@users.noreply.github.com>
2025-10-18 18:04:39 -0300

    feat: add dynamic command loading

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,65 @@
 import { Client, GatewayIntentBits, Message } from 'discord.js';
-import dotenv from 'dotenv';
-import { balance } from './commands/balance.ts';
-import { daily } from './commands/daily.ts';
+import { fileURLToPath, pathToFileURL } from 'url';
 import './utils/database.ts';
+import dotenv from 'dotenv';
+import path from 'path';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+const commands: { [key: string]: (msg: Message) => Promise<void> } = {};
 
-const prefix = process.env.PREFIX || '!';
+async function loadCommands() {
+  const commandDir = path.join(__dirname, 'commands');
+  const files = fs
+    .readdirSync(commandDir)
+    .filter((file) => file.endsWith('.ts'));
 
-client.on('messageCreate', async (msg: Message) => {
-  if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+  for (const file of files) {
+    const commandName = path.parse(file).name;
+    const commandPath = pathToFileURL(path.join(commandDir, file)).href;
+    const commandModule = await import(commandPath);
+    const commandFn = commandModule[commandName] as
+      | ((msg: Message) => Promise<void>)
+      | undefined;
 
-  const [cmd] = msg.content.slice(prefix.length).trim().split(' ');
+    if (commandFn) commands[commandName] = commandFn;
+  }
+}
 
-  if (cmd === 'balance') await balance(msg);
-  else if (cmd === 'daily') await daily(msg);
-});
+async function main() {
+  await loadCommands();
 
-client.once('ready', () => console.log(`✅ Logado como ${client.user?.tag}`));
-client.login(process.env.TOKEN);
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ],
+  });
+
+  const prefix = process.env.PREFIX || '!';
+
+  client.on('messageCreate', async (msg: Message) => {
+    if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+
+    const [cmdRaw] = msg.content.slice(prefix.length).trim().split(' ');
+    const cmd = cmdRaw?.toLowerCase();
+
+    if (!cmd || !(cmd in commands)) return;
+
+    const command = commands[cmd];
+    if (command) {
+      await command(msg);
+    }
+  });
+
+  client.once('clientReady', () =>
+    console.log(`✅ Logado como ${client.user?.tag}`)
+  );
+  client.login(process.env.TOKEN);
+}
+
+main();
