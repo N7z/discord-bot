30f735537cfb54c169719f16055102b8dd9472b6
Paulo Junior <73448096+N7z@users.noreply.github.com>
2025-10-19 00:09:59 -0300

    refactor(balance.ts): now users can see eachother's balances

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -4,7 +4,7 @@ import { Message } from 'discord.js';
 export const aliases: string[] = ['bal', 'saldo', 'carteira'];
 
 export async function balance(msg: Message) {
-  const userId = msg.author.id;
+  const userId = msg.mentions.users.first()?.id || msg.author.id;
   const user = await getUser(userId);
   msg.reply(`💰 | Carteira: ${user.balance}\n🏦 | Investido: ${user.invested}`);
 }
